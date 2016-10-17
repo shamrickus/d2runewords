17764376fd9a33f6bfb1d7323d26dc6d2a8bb7d6
Hamrick <steve.hamrick@tylertech.com>
2016-10-17 15:57:53 -0600

    Added tags, using custom filtering, advanced lvl formatting supported, multiple runes supported

--- app.js
+++ app.js
@@ -137,12 +137,22 @@ angular.module("mainApp", [])
 				lvl: 69
 			}
 	};
+	$scope.math = {
+		'<': function(x, y) { return x < y },
+		'<=': function(x , y) { return x <= y},
+		'>': function(x, y) { return x > y},
+		'>=': function(x, y) { return x >= y}
+	}
 
 	$scope.initItems = function(){
 		for(var i = 0; i < $scope.items.length; ++i){
 			let item = $scope.items[i];
 			item.lvlReq = $scope.getLvlReq(item.runes);
 		}
+		$("[placeholder='Runes']:not('bound')").change(function(){
+			$scope.$apply();
+		}).addClass("bound");
+
 	}
 
 	$scope.outputRune = function(runes){
@@ -180,5 +190,55 @@ angular.module("mainApp", [])
 		else{
 			return item.name;
 		}
+	}
+
+	$scope.checkFilters = function(testItem){
+		let name = testItem.name != undefined ? testItem.name.toLowerCase() : "";
+		let testName = $scope.name != undefined ? $scope.name.toLowerCase() : "";
+		if(!name.includes(testName)) return false;
+
+		let sockets = testItem.sockets != undefined ? testItem.sockets : "";
+		let testSockets = $scope.sockets != undefined ? $scope.sockets : "";
+		if(testSockets != "" && sockets != testSockets) return false;
+
+		let type = testItem.itemType != undefined ? testItem.itemType.toLowerCase() : "";
+		let testType = $scope.itemType != undefined ? $scope.itemType.toLowerCase() : "";
+		if(!type.includes(testType)) return false;
+
+		$scope.runes = $("#runes").val().toLowerCase().split(",");
+		let runes = testItem.runes.map(function(x){ return x.toLowerCase();});
+		for(var i = 0; i < $scope.runes.length; ++i){
+			let rune = $scope.runes[i];
+			if(rune === "") continue;
+			if(runes.indexOf(rune) == -1) return false;
+		}
+
+		let lvl = testItem.lvlReq.toString();
+		let testLvl = $scope.lvlReq != undefined ? $scope.lvlReq : "";
+
+		let mathRegex = new RegExp(/(((\<\=|\<)\s[0-9]+)|((\>\=|\>)\s[0-9]+))/);
+		if(testLvl != ""){
+			if(mathRegex.test(testLvl)){
+				var data = testLvl.split(" ");
+				if(!$scope.math[data[0]](lvl, data[1])) return false;
+			}
+			else if(!lvl.includes(testLvl)) return false;
+		}
+
+
+		let properties = testItem.properties.map(function(val){return val.toLowerCase();});
+		let testProps = $scope.properties != undefined ? $scope.properties.toLowerCase() : "";
+		let propFound = true;
+		if(testProps != ""){
+			propFound = false;
+			for(var i = 0; i < properties.length; ++i){
+				let property = properties[i];
+				if(property.includes(testProps)){
+					propFound = true;
+					break;
+				}
+			}
+		}
+		return propFound;
 	}
 }]);	
