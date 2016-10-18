f2ea9bf61288c49d0ca768dcced545aa57ec108a
Hamrick <steve.hamrick@tylertech.com>
2016-10-17 18:27:55 -0600

    Syncing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ angular.module("mainApp", [])
 	$scope.sortName = 'name';
 	$scope.sortReverse = false;
 	$scope.items = data;
-	$scope.runes = {
+	$scope.initRunes = {
 		"El": 
 			{
 				lvl: 11
@@ -137,12 +137,56 @@ angular.module("mainApp", [])
 				lvl: 69
 			}
 	};
+	$scope.math = {
+		'<': function(x, y) { return x < y },
+		'<=': function(x , y) { return x <= y},
+		'>': function(x, y) { return x > y},
+		'>=': function(x, y) { return x >= y}
+	}
+
+	$scope.substringMatcher = function(strs) {
+	  return function findMatches(q, cb) {
+	    var matches, substringRegex;
+
+	    // an array that will be populated with substring matches
+	    matches = [];
+
+	    // regex used to determine if a string contains the substring `q`
+	    substrRegex = new RegExp(q, 'i');
+
+	    // iterate through the pool of strings and for any string that
+	    // contains the substring `q`, add it to the `matches` array
+	    $.each(strs, function(i, str) {
+	      if (substrRegex.test(str)) {
+	        matches.push(str);
+	      }
+	    });
+
+	    cb(matches);
+	  };
+	};
 
 	$scope.initItems = function(){
 		for(var i = 0; i < $scope.items.length; ++i){
 			let item = $scope.items[i];
 			item.lvlReq = $scope.getLvlReq(item.runes);
 		}
+		$("[placeholder='Runes']:not('.bound')").change(function(){
+			$scope.$apply();
+		}).addClass("bound");
+
+		$("[data-toggle='tooltip']:not('.bound')").each(function(){
+			$(this).tooltip();
+		}).addClass("bound");
+
+		$("#runes").tagsinput({
+			confirmKeys: [13, 188, 32],
+			typeaheadjs: {
+				hint: true,
+				highlight: true,
+				source: $scope.substringMatcher(Object.keys($scope.initRunes))
+			}
+		});
 	}
 
 	$scope.outputRune = function(runes){
@@ -166,7 +210,7 @@ angular.module("mainApp", [])
 
 		for(var i = 0; i < runes.length; ++i){
 			let rune = runes[i];
-			let lvlReq = $scope.runes[rune].lvl;
+			let lvlReq = $scope.initRunes[rune].lvl;
 			if(lvlReq > minLvl) minLvl = lvlReq;
 		}
 
@@ -180,5 +224,55 @@ angular.module("mainApp", [])
 		else{
 			return item.name;
 		}
+	}
+
+	$scope.checkFilters = function(testItem){
+		let name = testItem.name != undefined ? testItem.name.toLowerCase() : "";
+		let testName = $scope.name != undefined ? $scope.name.toLowerCase() : "";
+		if(!name.includes(testName)) return false;
+
+		let sockets = testItem.sockets != undefined ? testItem.sockets : "";
+		let testSockets = $scope.sockets != undefined ? $scope.sockets : "";
+		if(testSockets != "" && sockets != testSockets) return false;
+
+		let type = testItem.itemType != undefined ? testItem.itemType.toLowerCase() : "";
+		let testType = $scope.itemType != undefined ? $scope.itemType.toLowerCase() : "";
+		if(!type.includes(testType)) return false;
+
+		$scope.runes = $("#runes").val().toLowerCase().split(",");
+		let runes = testItem.runes.map(function(x){ return x.toLowerCase();});
+		for(var i = 0; i < $scope.runes.length; ++i){
+			let rune = $scope.runes[i];
+			if(rune === "") continue;
+			if(runes.indexOf(rune) == -1) return false;
+		}
+
+		let lvl = testItem.lvlReq.toString();
+		let testLvl = $scope.lvlReq != undefined ? $scope.lvlReq : "";
+
+		let mathRegex = new RegExp(/(((\<\=|\<)\s[0-9]+)|((\>\=|\>)\s[0-9]+))/);
+		if(testLvl != ""){
+			if(mathRegex.test(testLvl)){
+				var data = testLvl.split(" ");
+				if(!$scope.math[data[0]](lvl, data[1])) return false;
+			}
+			else if(!lvl.includes(testLvl)) return false;
+		}
+
+
+		let properties = testItem.properties.map(function(val){return val.toLowerCase();});
+		let testProps = $scope.properties != undefined ? $scope.properties.toLowerCase() : "";
+		let propFound = true;
+		if(testProps != ""){
+			propFound = false;
+			for(var i = 0; i < properties.length; ++i){
+				let property = properties[i];
+				if(property.includes(testProps)){
+					propFound = true;
+					break;
+				}
+			}
+		}
+		return propFound;
 	}
 }]);	
